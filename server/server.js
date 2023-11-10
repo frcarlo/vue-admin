6b558295d5a83474527f22c1eabc075ff109e47e
Francesco Carlo <frcarlo@gmail.com>
2023-11-10 09:51:51 +0100

    cors channged

--- server/server.js
+++ server/server.js
@@ -15,10 +15,19 @@ const appPath = __dirname + '/app/';
 const http = require('http');
 const server = http.createServer(app);
 const {Server} = require("socket.io");
+const whitelist = ["http://loalhost:3001", "/", "http://localhost:3001"]
 const io = new Server(server,
     {
         cors: {
-            origin: 'http://localhost:3001',
+            origin: function (origin, callback) {
+                
+                if (whitelist.indexOf(origin) !== -1) {
+
+                    callback(null, true)
+                } else {
+                    callback(new Error('Not allowed by CORS'))
+                }
+            },
             credentials: true
         }
     }
@@ -31,7 +40,7 @@ const option = {
 
 const mongoURI = process.env.MONGODB_URI;
 mongoose
-    .connect("mongodb://localhost:27017/?authMechanism=DEFAULT", option)
+    .connect(`mongodb://${mongoURI}/?authMechanism=DEFAULT`, option)
     .then(
         function () {
             //connected successfully
