85087e803cd1cb2ce48dd51f4f3cf60fc570620f
Francesco Carlo <frcarlo@gmail.com>
2023-11-10 22:10:59 +0100

    build.sh added

--- client/vite.config.js
+++ client/vite.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
     },
     extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
-  base: process.env.MODE === "production" ? "/vue-admin/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/vue-admin/" : "/",
   build: {
     outDir: "../server/app",
   },
