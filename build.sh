c57d4485c0a522d70eacc20975fb904fd5e1b0e2
Francesco Carlo <frcarlo@gmail.com>
2023-11-10 21:43:57 +0100

    cors channged

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-cd client && npm run build && cd - || exit
+cd client && npm i && npm run build && cd - || exit
 
-cd server && npm run build && cd - || exit
+cd server && npm i && npm run build && cd - || exit
